cb02fc76dd9e3167ebee96c411d064c86354635a
Jieun Oh <65931227+0909oje@users.noreply.github.com>
2022-09-14 00:04:50 +0900

    feat : move the list : doing <-> done

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 const todoForm = document.querySelector('.todo-form');
 const todoInput = document.querySelector('.todo-input');
 const todoBtn = document.querySelector('.enter-button');
-const todoList = document.querySelector('.doing-list');
+const doingList = document.querySelector('.doing-list');
+const doneList = document.querySelector('.done-list');
 
-let id = 0;
+let doingID = 0;
+let doneID = 0;
 
-const deleteTodo = (e) => {
+const deleteDoing = (e) => {
   const removeList = e.target.parentElement;
   removeList.remove();
+  doingID--;
 };
 
-const paintTodos = () => {
-  id++;
+const deleteDone = (e) => {
+  const removeList = e.target.parentElement;
+  removeList.remove();
+  doneID--;
+};
+
+const moveToDoing = (e) => {
+  doingID++;
+  const doingText = e.target.parentElement.children[0].textContent;
 
   const li = document.createElement('li');
   li.classList.add('todo-item');
 
   const text = document.createElement('Text');
   text.classList.add('todo-text');
-  text.innerText = `${id} : ${todoInput.value}`;
+  text.innerText = doingText;
   todoInput.value = '';
 
   const button = document.createElement('button');
@@ -28,9 +38,63 @@ const paintTodos = () => {
   li.appendChild(text);
   li.appendChild(button);
 
-  todoList.appendChild(li);
+  doingList.appendChild(li);
 
-  button.addEventListener('click', deleteTodo);
+  button.addEventListener('click', deleteDone);
+  text.addEventListener('click', moveToDone);
+
+  deleteDoing(e);
+};
+
+const moveToDone = (e) => {
+  doneID++;
+  const doneText = e.target.parentElement.children[0].textContent;
+
+  const li = document.createElement('li');
+  li.classList.add('todo-item');
+
+  const text = document.createElement('Text');
+  text.classList.add('todo-text');
+  text.innerText = doneText;
+  todoInput.value = '';
+
+  const button = document.createElement('button');
+  button.classList.add('delete-button');
+  button.innerText = 'X';
+
+  li.appendChild(text);
+  li.appendChild(button);
+
+  doneList.appendChild(li);
+
+  button.addEventListener('click', deleteDone);
+  text.addEventListener('click', moveToDoing);
+
+  deleteDoing(e);
+};
+
+const paintTodos = () => {
+  doingID++;
+
+  const li = document.createElement('li');
+  li.classList.add('todo-item');
+
+  const text = document.createElement('Text');
+  text.classList.add('todo-text');
+  text.innerText = todoInput.value;
+  todoInput.value = '';
+
+  const button = document.createElement('button');
+  button.classList.add('delete-button');
+  button.innerText = 'X';
+
+  li.appendChild(text);
+  li.appendChild(button);
+
+  doingList.appendChild(li);
+
+  button.addEventListener('click', deleteDoing);
+  text.addEventListener('click', moveToDone);
 };
 
 function handleToDoSubmit(e) {
