73684983eea041eb53bf7b6823dda7f60251d530
Chaeyeon An <90609254+chaeyeonan@users.noreply.github.com>
2022-09-15 01:31:25 +0900

    fix: delete completed task

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //오늘 날짜
 const week=['월','화','수','목','금','토','일']
 let nowDate =new Date().getFullYear()+'년 '+(new Date().getMonth()+1)+'월 '+new Date().getDate()+'일 '+week[new Date().getDay()-1]+'요일';
-document.getElementsByClassName('today')[0].innerHTML=now;
+document.getElementsByClassName('today')[0].innerHTML=nowDate;
 
 let todoNum=0,index=0,doneNum=0;
 //할 일 추가 (클래스 네임을 변수로 다르게 설정하여 추가/삭제 기능)
@@ -13,6 +13,7 @@ function getValueInput(){
         <div id="todo-task" class="todo-task-${index}">${inputValue.value}</div> 
         <img src="./img/delete.png" class="image" onclick="todo_delete(${index})" /> 
     </div>`); 
+    console.log(index);
     inputValue.value='';
 
     //남은 할 일 개수 표시
@@ -31,8 +32,9 @@ function done(i){
     <div id="done-task-container" class="done-task-container-${i}">
         <img src="./img/checkmark.png" class="image_done" onclick="doneDelete(${i})" /> 
         <div id="done-task" class="done-task-${i}">${value}</div> 
-        <img src="./img/delete.png" class="image" onclick="done_delete(${index})" /> 
+        <img src="./img/delete.png" class="image" onclick="done_delete(${i})" /> 
     </div>`); 
+    console.log(i);
 
     //할 일에서 삭제
     todo_delete(i);
@@ -43,6 +45,7 @@ function done(i){
 }
 document.getElementsByClassName('done-title')[0].innerHTML='완료한 일 '+0+'개';
 
+
 //완료 목록에서 삭제 & 남은 할 일 추가
 function doneDelete(i){
 
@@ -52,7 +55,7 @@ function doneDelete(i){
     <div id="task-container" class="task-container-${i}">
         <img src="./img/circle.png" class="image_todo" onclick="done(${i})" /> 
         <div id="todo-task" class="todo-task-${i}">${value}</div> 
-        <img src="./img/delete.png" class="image" onclick="todo_delete(${index})" /> 
+        <img src="./img/delete.png" class="image" onclick="todo_delete(${i})" /> 
     </div>`); 
 
     //남은 할 일 개수 표시
@@ -62,7 +65,6 @@ function doneDelete(i){
     //완료 목록에서 삭제
     done_delete(i);
 }
-
 
 //할 일에서 삭제
 function todo_delete(i){
