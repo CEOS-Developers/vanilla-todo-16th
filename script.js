87acdf98f8932540738e0cac357d55f672211b76
Chaeyeon An <90609254+chaeyeonan@users.noreply.github.com>
2022-09-15 00:52:13 +0900

    feat: seperate complete/incomplete task

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ let nowDate =new Date().getFullYear()+'년 '+(new Date().getMonth()+1)+'월 '+ne
 document.getElementsByClassName('today')[0].innerHTML=now;
 
 let todoNum=0,index=0,doneNum=0;
-//할 일 추가
+//할 일 추가 (클래스 네임을 변수로 다르게 설정하여 추가/삭제 기능)
 function getValueInput(){
-    var inputValue = document.getElementById('inputValue');
+    var inputValue = document.getElementById('inputValue'); 
     document.querySelector(`.container`).insertAdjacentHTML('beforeend',` 
-    <div id="task-container" class="task-container-${++index}">
-        <img src="./img/circle.png" class="image_todo" onclick="done(${index})" /> 
+    <div id="task-container" class="task-container-${++index}"> 
+        <img src="./img/circle.png" class="image_todo" onclick="done(${index})" />  
         <div id="todo-task" class="todo-task-${index}">${inputValue.value}</div> 
         <img src="./img/delete.png" class="image" onclick="todo_delete(${index})" /> 
     </div>`); 
@@ -20,3 +20,25 @@ function getValueInput(){
     document.getElementsByClassName('todo-title')[0].innerHTML='남은 할 일 '+todoNum+'개';
 }
 document.getElementsByClassName('todo-title')[0].innerHTML='남은 할 일 '+0+'개';
+
+
+//완료
+function done(i){
+
+    //완료 목록에 추가
+    var value=document.getElementsByClassName("todo-task-"+i)[0].innerHTML;
+    document.querySelector(`.done-container`).insertAdjacentHTML('beforeend',` 
+    <div id="done-task-container" class="done-task-container-${i}">
+        <img src="./img/checkmark.png" class="image_done" onclick="doneDelete(${i})" /> 
+        <div id="done-task" class="done-task-${i}">${value}</div> 
+        <img src="./img/delete.png" class="image" onclick="done_delete(${index})" /> 
+    </div>`); 
+
+    //할 일에서 삭제
+    todo_delete(i);
+
+    //완료 개수 표시
+    doneNum=document.getElementsByClassName('image_done').length;
+    document.getElementsByClassName('done-title')[0].innerHTML='완료한 일 '+doneNum+'개';
+}
+document.getElementsByClassName('done-title')[0].innerHTML='완료한 일 '+0+'개';
