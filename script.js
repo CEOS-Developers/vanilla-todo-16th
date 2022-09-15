cbcfb2a8bf0ad8fa170d14fd308f67bf30283546
Yoo SeonHo <tjsgh503@naver.com>
2022-09-15 16:21:59 +0900

    feat : make delete btn in Doing and make deleteToDo func

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const toDo = document.getElementById('toDoForm');
 const toDoInput = document.getElementById('toDoInput');
-const toDoList = document.getElementsByClassName('doing');
+const toDoList = document.getElementById('doing');
+//getElementsByClassName ==> 반환을 HTMLCollection으로, getElementById => 요소를 그대로 반환!!!
+
+function deleteToDo(event) {
+  const deleteItem = event.target.parentElement;
+  deleteItem.remove();
+}
 
 function Doing(saveToDo) {
   const content = document.createElement('li');
+  const btn = document.createElement('button');
+  btn.innerText = 'X';
+  btn.addEventListener('click', deleteToDo);
   content.innerText = saveToDo;
-  toDoList[0].appendChild(content);
+  content.appendChild(btn);
+  toDoList.appendChild(content);
 }
 
 function onSubmit(event) {
