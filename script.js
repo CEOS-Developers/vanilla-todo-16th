e8f1e88d66a85016dd410db4a42cceeb304815b2
seondal <sseondal@gmail.com>
2022-09-15 17:21:16 +0900

    [Feat] submitTodo()

---
+++ script.js
@@ -0,0 +1,28 @@
+const todoForm = document.getElementById("todo-form");
+const todoInput = todoForm.querySelector("input");
+const todoList = document.getElementById("todo-list");
+
+todoForm.addEventListener("submit", submitTodo);
+
+function submitTodo(e) {
+  e.preventDefault();
+
+  const todoLi = document.createElement("li");
+  const todoSpan = document.createElement("span");
+  const deleteBtn = document.createElement("button");
+
+  todoSpan.innerText = todoInput.value;
+  todoInput.value = "";
+
+  deleteBtn.innerText = "❌";
+  deleteBtn.addEventListener("click", deleteTodo);
+
+  todoLi.appendChild(todoSpan);
+  todoLi.appendChild(deleteBtn);
+
+  todoList.append(todoLi);
+}
+
+function deleteTodo() {
+  console.log("delete");
+}
