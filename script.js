73a6c4373892babd41ffbc3cb6469a9809e3be49
seondal <sseondal@gmail.com>
2022-09-15 19:56:58 +0900

    [Feat] returnTodo()

--- script.js
+++ script.js
@@ -5,15 +5,12 @@ const doneList = document.getElementById("done-list");
 
 todoForm.addEventListener("submit", submitTodo);
 
-function submitTodo(e) {
-  e.preventDefault();
-
+function createTodo(content) {
   const todoLi = document.createElement("li");
   const todoSpan = document.createElement("span");
   const deleteBtn = document.createElement("button");
 
-  todoSpan.innerText = todoInput.value;
-  todoInput.value = "";
+  todoSpan.innerText = content;
 
   deleteBtn.innerText = "✅";
   deleteBtn.addEventListener("click", completeTodo);
@@ -24,23 +21,38 @@ function submitTodo(e) {
   todoList.append(todoLi);
 }
 
+function submitTodo(e) {
+  e.preventDefault();
+
+  createTodo(todoInput.value);
+}
+
 function completeTodo(e) {
   const target = e.target.parentElement;
 
   const doneLi = document.createElement("li");
   const doneSpan = document.createElement("span");
   const doneBtn = document.createElement("button");
+  const returnBtn = document.createElement("button");
 
   doneSpan.innerText = target.querySelector("span").innerText;
   doneBtn.innerText = "❌";
   doneBtn.addEventListener("click", deleteTodo);
-
+  returnBtn.addEventListener("click", returnTodo);
+  returnBtn.innerText = "🆙";
   doneLi.appendChild(doneSpan);
   doneLi.appendChild(doneBtn);
+  doneLi.appendChild(returnBtn);
 
   doneList.append(doneLi);
 
   target.remove();
+}
+
+function returnTodo(e) {
+  const value = e.target.parentElement.querySelector("span").innerText;
+  deleteTodo(e);
+  createTodo(value);
 }
 
 function deleteTodo(e) {
