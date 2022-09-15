779869d08786213502cb38f52245bc9860b51871
heesoo <11junghs@gmail.com>
2022-09-16 07:43:40 +0900

    feat : todo <-> done

--- script.js
+++ script.js
@@ -1,30 +1,83 @@
 const toDoForm = document.querySelector(".toDoForm");
 const toDos = document.querySelector(".toDos");
 const toDoInput= toDoForm.querySelector("input");
+const done = document.querySelector(".done");
 
 let toDoList = [];
-let number = 0;
+let doneList = [];
 
-// toDoList 저장 함수 
-function pushToDo(toDo){
-    const toDoObj = {
-        num : number +1,
-        text: toDo,
-    };
-    number+=1;
-    toDoList.push(toDoObj);
-    console.log('push:',toDoList);
+// todo, done 저장함수
+function addFunc(text,id,type){
+    const addObj = {
+        num : id,
+        text : text,
+        type : type
+    }
+    if(type=="todo"){
+        toDoList.push(addObj);
+    }else{
+        doneList.push(addObj);
+    }
+    console.log('todolist:',toDoList);
+    console.log('donelist:',doneList);
 }
 
+
+// todolist에서 삭제
 function delToDo(event){
     const button = event.target;
     const li = button.parentNode;
     toDos.removeChild(li);
-    console.log(li.id)
     toDoList = toDoList.filter((toDo)=> toDo.num !== Number(li.id));
     console.log('del:',toDoList);
 }
 
+// donelist에서 삭제
+function delDone(event){
+    const span = event.target;
+    const li = span.parentNode;
+    done.removeChild(li);
+    doneList = doneList.filter((done)=>done.num !== Number(li.id));
+}
+
+// done에 추가
+function addDone(text){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const delButton = document.createElement("button");
+
+    delButton.innerText="X"
+    delButton.addEventListener("click",delDone);
+
+    span.innerHTML = text;
+    span.addEventListener("click",changeDone);
+
+    li.appendChild(span);
+    li.appendChild(delButton);
+    li.id = doneList.length +1;
+    done.appendChild(li);
+    addFunc(text,doneList.length +1,"done");
+}
+
+// todo -> done
+function changeToDo(event){
+    const span = event.target;
+    const li = span.parentNode;
+    console.log('changeToDo:',span.innerHTML);
+    delToDo(event);
+    addDone(span.innerHTML);
+}
+
+// done -> todo
+function changeDone(event){
+    const span = event.target;
+    const li = span.parentNode;
+    console.log('changeDone:',span.innerHTML);
+    delDone(event);
+    addToDo(span.innerHTML);
+}
+
+// todo에 추가
 function addToDo(toDo){
     const li = document.createElement("li");
     const span = document.createElement("span");
@@ -34,11 +87,13 @@ function addToDo(toDo){
     delButton.addEventListener("click",delToDo);
 
     span.innerHTML = toDo;
+    span.addEventListener("click",changeToDo);
+
     li.appendChild(span);
     li.appendChild(delButton);
-    li.id = number+1; //toDo list 관리 위해서 id 추가
+    li.id = toDoList.length +1; // list 관리 위해서 id 추가
     toDos.appendChild(li);
-    pushToDo(toDo);
+    addFunc(toDo,toDoList.length +1,"todo");
 }
 
 function createToDo(event){
