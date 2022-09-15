21e77cfbe033e9229b7f71bc1f46b03020abe105
Yoo SeonHo <tjsgh503@naver.com>
2022-09-15 16:51:01 +0900

    feat : count doing

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const toDo = document.getElementById('toDoForm');
 const toDoInput = document.getElementById('toDoInput');
 const toDoList = document.getElementById('doing');
 //getElementsByClassName ==> 반환을 HTMLCollection으로, getElementById => 요소를 그대로 반환!!!
+const doingText = document.getElementById('doingText');
+const doneText = document.getElementById('doneText');
 
 function deleteToDo(event) {
   const deleteItem = event.target.parentElement;
   deleteItem.remove();
+  doingText.innerText = `Doing (${toDoList.childElementCount})`;
 }
 
 function Doing(saveToDo) {
@@ -16,6 +19,7 @@ function Doing(saveToDo) {
   content.innerText = saveToDo;
   content.appendChild(btn);
   toDoList.appendChild(content);
+  doingText.innerText = `Doing (${toDoList.childElementCount})`;
 }
 
 function onSubmit(event) {
